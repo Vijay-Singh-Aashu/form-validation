6c5aafc2e9a421bd5f287540a192653ce5b9ccf6
Vijay Singh <vijaysinghaashu@gmail.com>
2023-10-30 10:37:08 +0530

    base-code-created

---
+++ src/main/java/com/excellence_spring_boot_form_validation/SpringBootFormValidationApplication.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_form_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFormValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFormValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence_spring_boot_form_validation/SpringBootFormValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_form_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
